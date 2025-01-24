115ef303ce22eef1705a5b3b856059eed3ed8093
Champifloyd <77200272+DIEGONUSKU@users.noreply.github.com>
2025-01-24 13:00:45 +0100

    Add files via upload

---
+++ src/main/java/net/irene/amorcito/worldgen/ModBiomeModifiers.java
@@ -0,0 +1,40 @@
+package net.irene.amorcito.worldgen;
+
+import net.irene.amorcito.Amorcito;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.common.world.ForgeBiomeModifiers;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.List;
+
+public class ModBiomeModifiers {
+
+    public static final ResourceKey<BiomeModifier> ADD_KALANCHOE = registerKey("add_kalanchoe");
+
+    public static void bootstrap(BootstrapContext<BiomeModifier> context) {
+        var placedFeature = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+
+        context.register(ADD_KALANCHOE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                HolderSet.direct(biomes.getOrThrow(Biomes.FOREST), biomes.getOrThrow(Biomes.BIRCH_FOREST)),
+                HolderSet.direct(placedFeature.getOrThrow(ModPlacedFeatures.KALANCHOE_PLACED_KEY)),
+                GenerationStep.Decoration.VEGETAL_DECORATION));
+
+    }
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, ResourceLocation.fromNamespaceAndPath(Amorcito.MODID, name));
+    }
+}
